c4ac1492c04fda5b96015be998801fb45b5380ba
Jonah Liss <52020219+jonahliss@users.noreply.github.com>
2021-12-01 01:42:51 -0500

    Create Scrape Ubet.py

---
+++ Scrape Ubet.py
@@ -0,0 +1,71 @@
+import time
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+
+def appendDataTeams(html_list, new_list):
+    for item in html_list:
+        new_list.append(item.text)
+
+def appendDataSpreads(html_list, new_list):
+    i = 0
+    for item in html_list[1:]:
+        if i % 3 == 0:
+            new_list.append(item.text)
+        i += 1
+
+def appendDataOdds(html_list, new_list):
+    i = 0
+    for item in html_list[3:]:
+        if i % 3 == 0:
+            new_list.append(item.text)
+        i += 1
+       
+class SportDynamic:
+    def __init__(self, url, league):
+        self.url = url
+        self.league = league
+        self.teams_html = []
+        self.spreads_html = []
+        self.odds_html = []
+        self.teams_list = []
+        self.spreads_list = []
+        self.odds_list = []
+    def launchDriver(self):
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+        self.driver.get(self.url)
+    def enterDriver(self):
+        self.driver.find_element(By.NAME, "Account").send_keys("Efind1")
+        self.driver.find_element(By.NAME, "Password").send_keys("123")
+        self.driver.find_element(By.XPATH, '//button[text()=" Login"]').click()
+    def navigateDriver(self):
+        self.driver.find_element(By.ID, "ctl00_lnkSports").click()
+        self.driver.find_element(By.LINK_TEXT, self.league).click()
+        self.driver.find_element(By.ID, "continue-2").click()
+    def retrieveData(self):
+        html = self.driver.page_source
+        soup = BeautifulSoup(html, "html.parser")
+        self.teams_html = soup.find_all(class_='text-black')
+        self.spreads_html = soup.find_all(class_='btn btn-danger')
+        self.odds_html = soup.find_all(class_='btn btn-danger')
+    def sortData(self):
+        appendDataTeams(self.teams_html, self.teams_list)
+        appendDataSpreads(self.spreads_html, self.spreads_list)
+        appendDataOdds(self.odds_html, self.odds_list)
+    def presentData(self):
+        df = pd.DataFrame()
+        df = pd.concat([df, pd.DataFrame({'Teams':self.teams_list}),pd.DataFrame({'Spreads':self.spreads_list}),
+                pd.DataFrame({'Odds':self.odds_list})], axis=1)
+        print(df)
+        
+  NBA = SportDynamic('https://ubet.ag/', 'NCAA FOOTBALL')
+  NBA.launchDriver()
+  NBA.enterDriver()
+  NBA.navigateDriver()
+  NBA.retrieveData()
+  NBA.sortData()
+  NBA.presentData()
