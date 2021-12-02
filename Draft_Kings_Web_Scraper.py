13687cd4c8e4b0a27fd16e7e52fc3f5c504ac048
chrzhang <chrzhang@umich.edu>
2021-12-02 10:44:21 -0500

    Reformat spreadsheet for just ubet and draftkings

--- Draft_Kings_Web_Scraper.py
+++ Draft_Kings_Web_Scraper.py
@@ -14,16 +14,16 @@ def appendDataSpreads(html_list, new_list):
             new_list.append(item)
         i += 1
 
-        
+
 def appendDataOdds(html_list, new_list):
     for item in html_list:
         new_list.append(item.text)
 
-        
+
 def appendDataTeams(html_list, new_list):
     for item in html_list:
         new_list.append(item.text)
-     
+
 
 class SportStatic:
     def __init__(self, URL):
@@ -37,21 +37,27 @@ class SportStatic:
         self.odds = []
         self.teams = []
         self.df = []
+
     def retrieveData(self):
         self.page = requests.get(self.url)
         self.soup = BeautifulSoup(self.page.content, "html.parser")
         self.spreads_html = self.soup.find_all(class_='sportsbook-outcome-body-wrapper')
         self.odds_html = self.soup.find_all(class_='sportsbook-odds american no-margin default-color')
         self.teams_html = self.soup.find_all(class_='event-cell__name-text')
+
     def sortData(self):
         appendDataSpreads(self.spreads_html, self.spreads)
         appendDataOdds(self.odds_html, self.odds)
         appendDataTeams(self.teams_html, self.teams)
+
     def presentData(self):
+        self.retrieveData()
+        self.sortData()
         self.df = pd.DataFrame()
-        self.df = pd.concat([self.df, pd.DataFrame({'Teams':self.teams}),pd.DataFrame({'Spreads':self.spreads}),
-                            pd.DataFrame({'Odds':self.odds})], axis=1)
-        print(self.df)
+        self.df = pd.concat([self.df, pd.DataFrame({'Teams': self.teams}), pd.DataFrame({'Spreads': self.spreads}),
+                             pd.DataFrame({'Odds': self.odds})], axis=1)
+        return self.df
+
 
 '''
 # Draft Kings NFL
--- df_To_Google_Sheets.py
+++ df_To_Google_Sheets.py
@@ -13,13 +13,13 @@ gc = gspread.oauth(
 )
 
 sh = gc.open("BettingScraper")
-worksheetNFL = sh.get_worksheet(0)
+worksheetNFL = sh.get_worksheet(2)
 # sh.add_worksheet(title="CFB", rows=50, cols=30)
-worksheetCFB = sh.get_worksheet(1)
+worksheetCFB = sh.get_worksheet(3)
 # sh.add_worksheet(title="NBA", rows=50, cols=10)
-worksheetNBA = sh.get_worksheet(2)
+worksheetNBA = sh.get_worksheet(4)
 # sh.add_worksheet(title="CBB", rows=100, cols=10)
-worksheetCBB = sh.get_worksheet(3)
+worksheetCBB = sh.get_worksheet(5)
 
 
 # check if worksheet works
@@ -30,7 +30,7 @@ draftkings_classes = ['sportsbook-outcome-cell__line','sportsbook-odds american 
 # grab NFL worksheet
 worksheetNFL.clear()
 
-NFL = dkweb.SportStatic("https://sportsbook.draftkings.com/leagues/football/88670561", draftkings_classes)
+NFL = dkweb.SportStatic("https://sportsbook.draftkings.com/leagues/football/88670561")
 df = NFL.presentData()
 worksheetNFL.update([df.columns.values.tolist()] + df.values.tolist())
 
@@ -40,35 +40,35 @@ worksheetNFL.update([df.columns.values.tolist()] + df.values.tolist())
 
 NFL = ubweb.SportDynamic('https://ubet.ag/', 'NFL')
 ubdfNFL = NFL.presentData()
-worksheetNFL.update('G:I', [ubdfNFL.columns.values.tolist()] + ubdfNFL.values.tolist())
+worksheetNFL.update('D:F', [ubdfNFL.columns.values.tolist()] + ubdfNFL.values.tolist())
 
-NFL = wwweb.SportDynamic('https://www.wagerwizard.ag/Logins/007/sites/wagerwizard/index.aspx', "\'SportClick(\"11,1\",this)\'")
-wwdfNFL = NFL.presentData()
-worksheetNFL.update('J:L', [wwdfNFL.columns.values.tolist()] + wwdfNFL.values.tolist())
+# NFL = wwweb.SportDynamic('https://www.wagerwizard.ag/Logins/007/sites/wagerwizard/index.aspx', "\'SportClick(\"11,1\",this)\'")
+# wwdfNFL = NFL.presentData()
+# worksheetNFL.update('J:L', [wwdfNFL.columns.values.tolist()] + wwdfNFL.values.tolist())
 
 # # grab CFB worksheet
 worksheetCFB.clear()
-CFB = dkweb.SportStatic("https://sportsbook.draftkings.com/leagues/football/88670775", draftkings_classes)
+CFB = dkweb.SportStatic("https://sportsbook.draftkings.com/leagues/football/88670775")
 dfCFB = CFB.presentData()
 worksheetCFB.update([dfCFB.columns.values.tolist()] + dfCFB.values.tolist())
 
 # CFB = stweb.SportDynamic('https://www.sundaytilt.com/', 'basketball', 'NBA ')
 # stdfCFB = CFB.presentData()
-# worksheetCFB.update('D1:', [stdfCFB.columns.values.tolist()] + stdfCFB.values.tolist())
+# worksheetCFB.update('D:F', [stdfCFB.columns.values.tolist()] + stdfCFB.values.tolist())
 
 CFB = ubweb.SportDynamic('https://ubet.ag/', 'NCAA FOOTBALL')
 ubdfCFB = CFB.presentData()
-worksheetCFB.update('G:I', [ubdfCFB.columns.values.tolist()] + ubdfCFB.values.tolist())
+worksheetCFB.update('D:F', [ubdfCFB.columns.values.tolist()] + ubdfCFB.values.tolist())
 
-# TODO fix the static string param
-CFB = wwweb.SportDynamic('https://www.wagerwizard.ag/Logins/007/sites/wagerwizard/index.aspx', "\'SportClick(\"12,1\",this)\'")
-wwdfCFB = CFB.presentData()
-worksheetCFB.update('J:L', [wwdfCFB.columns.values.tolist()] + wwdfCFB.values.tolist())
+# # TODO fix the static string param
+# CFB = wwweb.SportDynamic('https://www.wagerwizard.ag/Logins/007/sites/wagerwizard/index.aspx', "\'SportClick(\"12,1\",this)\'")
+# wwdfCFB = CFB.presentData()
+# worksheetCFB.update('J:L', [wwdfCFB.columns.values.tolist()] + wwdfCFB.values.tolist())
 
 # grab NBA worksheet
 worksheetNBA.clear()
 
-NBA = dkweb.SportStatic("https://sportsbook.draftkings.com/leagues/basketball/88670846", draftkings_classes)
+NBA = dkweb.SportStatic("https://sportsbook.draftkings.com/leagues/basketball/88670846")
 dfNBA = NBA.presentData()
 worksheetNBA.update('A:C', [dfNBA.columns.values.tolist()] + dfNBA.values.tolist())
 
@@ -78,16 +78,16 @@ worksheetNBA.update('A:C', [dfNBA.columns.values.tolist()] + dfNBA.values.tolist
 
 NBA = ubweb.SportDynamic('https://ubet.ag/', 'NBA')
 ubdfNBA = NBA.presentData()
-worksheetNBA.update('G:I', [ubdfNBA.columns.values.tolist()] + ubdfNBA.values.tolist())
+worksheetNBA.update('D:F', [ubdfNBA.columns.values.tolist()] + ubdfNBA.values.tolist())
 
-NBA = wwweb.SportDynamic('https://www.wagerwizard.ag/Logins/007/sites/wagerwizard/index.aspx', "\'SportClick(\"9,1\",this)\'")
-wwdfNBA = NBA.presentData()
-worksheetNBA.update('J:L', [wwdfNBA.columns.values.tolist()] + wwdfNBA.values.tolist())
+# NBA = wwweb.SportDynamic('https://www.wagerwizard.ag/Logins/007/sites/wagerwizard/index.aspx', "\'SportClick(\"9,1\",this)\'")
+# wwdfNBA = NBA.presentData()
+# worksheetNBA.update('J:L', [wwdfNBA.columns.values.tolist()] + wwdfNBA.values.tolist())
 
 # grab CBB worksheet
 worksheetCBB.clear()
 
-CBB = dkweb.SportStatic("https://sportsbook.draftkings.com/leagues/basketball/88670771", draftkings_classes)
+CBB = dkweb.SportStatic("https://sportsbook.draftkings.com/leagues/basketball/88670771")
 dfCBB = CBB.presentData()
 worksheetCBB.update([dfCBB.columns.values.tolist()] + dfCBB.values.tolist())
 
@@ -97,9 +97,8 @@ worksheetCBB.update([dfCBB.columns.values.tolist()] + dfCBB.values.tolist())
 
 CBB = ubweb.SportDynamic('https://ubet.ag/', 'NCAA BASKETBALL')
 ubdfCBB = CBB.presentData()
-worksheetCBB.update('G:I', [ubdfCBB.columns.values.tolist()] + ubdfCBB.values.tolist())
+worksheetCBB.update('D:F', [ubdfCBB.columns.values.tolist()] + ubdfCBB.values.tolist())
 
-CBB = wwweb.SportDynamic('https://www.wagerwizard.ag/Logins/007/sites/wagerwizard/index.aspx', "\'SportClick(\"10,1\",this)\'")
-wwdfCBB = CBB.presentData()
-worksheetCBB.update('J:L', [wwdfCBB.columns.values.tolist()] + wwdfCBB.values.tolist())
-
+# CBB = wwweb.SportDynamic('https://www.wagerwizard.ag/Logins/007/sites/wagerwizard/index.aspx', "\'SportClick(\"10,1\",this)\'")
+# wwdfCBB = CBB.presentData()
+# worksheetCBB.update('J:L', [wwdfCBB.columns.values.tolist()] + wwdfCBB.values.tolist())
