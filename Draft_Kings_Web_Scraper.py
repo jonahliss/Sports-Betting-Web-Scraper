c6ba22d70a042fe5535137964b9cebb0aae910dd
Jonah Liss <52020219+jonahliss@users.noreply.github.com>
2021-12-21 17:28:17 -0500

    Update Draft_Kings_Web_Scraper.py

--- Draft_Kings_Web_Scraper.py
+++ Draft_Kings_Web_Scraper.py
@@ -1,15 +1,44 @@
-import time
-
 import requests
 import gspread
 from bs4 import BeautifulSoup
 import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
 
 
 def getRange(index):
     if index / 26 >= 1:
         return chr(64 + (index // 26)) + chr(65 + (index % 26))
     return chr(65 + index)
+
+
+# PURPOSE: removes all special chracters from the key
+# PURPOSE: makes the key lowercase
+def formatKey(key):
+    key = key.lower()
+    key = key.replace("(", "")
+    key = key.replace(")", "")
+    key = key.replace(" - ", " ")
+    key = key.replace("1h", "1st half")
+    key = key.replace("2h", "2nd half")
+    key = key.replace("1q", "1st quarter")
+    key = key.replace("qtr", "quarter")
+    key = key.replace("2q", "2nd quarter")
+    key = key.replace("3q", "3rd quarter")
+    key = key.replace("4q", "4th quarter")
+    key = key.replace("bk", "basketball")
+    key = key.replace("b ", "basketball")
+    key = key.replace("fb", "football")
+    key = key.replace("lines", "")
+    return key
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 
 class SportStatic:
@@ -20,10 +49,11 @@ class SportStatic:
         self.odds_list = []
         self.moneylines_list = []
         self.df = []
-
+        
     def retrieveData(self):
-        page = requests.get(self.url)
-        self.soup = BeautifulSoup(page.content, "html.parser")
+        driver.get(self.url)
+        page = driver.page_source
+        self.soup = BeautifulSoup(page, "html.parser")
 
     def sortData(self):
         for event in self.soup.select('div.parlay-card-10-a'):
@@ -31,24 +61,49 @@ class SportStatic:
             for child in children[1:]:
                 try:
                     team = child.find_all(class_='event-cell__name-text')[0].text
-                    team = team.lower()
-                    team = team.strip()
                 except:
                     team = "NaN"
+                try:
+                    moneyline = child.find_all(class_='sportsbook-odds american no-margin default-color')[0].text
+                except:
+                    moneyline = "NaN"
+                try:
+                    temp = child.find_all(class_='sportsbook-outcome-body-wrapper')[1].text
+                    if (temp == moneyline):
+                        odds = "NaN"
+                    else:
+                        odds = temp
+                    spread = child.find_all(class_='sportsbook-outcome-body-wrapper')[0].text
+                except:
+                    try:
+                        temp = child.find_all(class_='sportsbook-outcome-body-wrapper')[0].text 
+                        if (temp[0] == 'O' or temp[0] == 'U'):
+                            spread = "NaN"
+                            odds = temp
+                        elif (temp == moneyline):
+                            spread = "NaN"
+                            odds = "NaN"
+                        else:
+                            spread = temp
+                            odds = "NaN"
+                    except:
+                        spread = "NaN"
+                        odds = "NaN"
+                
+                '''
                 try:
                     spread = child.find_all(class_='sportsbook-outcome-body-wrapper')[0].text
                 except:
                     spread = "NaN"
                 try:
-                    odds = child.find_all(class_='sportsbook-outcome-body-wrapper')[1].text
-                    odds = odds.replace('O ', 'o')
-                    odds = odds.replace('U ', 'u')
+                    odds = child.find_all(class_='sportsbook-outcome-body-wrapper')[1].text      
                 except:
                     odds = "NaN"
                 try:
                     moneyline = child.find_all(class_='sportsbook-outcome-body-wrapper')[2].text
                 except:
                     moneyline = "NaN"
+                '''
 
                 self.teams_list.append(team)
                 self.spreads_list.append(spread)
@@ -68,38 +123,30 @@ class SportStatic:
         self.retrieveData()
         self.sortData()
         
-
+ 
 gc = gspread.service_account(filename='credentials.json')
 print("Connected to Google Sheet")
 
 sh = gc.open("BettingScraper")
-worksheet = sh.get_worksheet(1)
+worksheet = sh.get_worksheet(5)
 
 
-while True:
-    
-    NFL = SportStatic("https://sportsbook.draftkings.com/leagues/football/88670561")
-    NFL.collectData()
-    dfNFL = NFL.displayData()
-    worksheet.update('B:E', [dfNFL.columns.values.tolist()] + dfNFL.values.tolist())
-    print('NFL Updated')
-    
-    CFB = SportStatic("https://sportsbook.draftkings.com/leagues/football/88670775")
-    CFB.collectData()
-    dfCFB = CFB.displayData()
-    worksheet.update('G:J', [dfCFB.columns.values.tolist()] + dfCFB.values.tolist())
-    print('CFB Updated')
-    
-    NBA = SportStatic("https://sportsbook.draftkings.com/leagues/basketball/88670846")
-    NBA.collectData()
-    dfNBA = NBA.displayData()
-    worksheet.update('L:O', [dfNBA.columns.values.tolist()] + dfNBA.values.tolist())
-    print('NBA Updated')
-    
-    CBB = SportStatic("https://sportsbook.draftkings.com/leagues/basketball/88670771")
-    CBB.collectData()
-    dfCBB = CBB.displayData()
-    worksheet.update('Q:T', [dfCBB.columns.values.tolist()] + dfCBB.values.tolist())
-    print('CBB Updated')
+scraping_list = [['A1','NFL','https://sportsbook.draftkings.com/leagues/football/88670561','B:E'],
+                 ['F1','NFL Halves','https://sportsbook.draftkings.com/leagues/football/88670561?category=halves','G:J'],
+                 ['K1','CFB','https://sportsbook.draftkings.com/leagues/football/88670775', 'L:O'],
+                 ['P1','CFB Halves','https://sportsbook.draftkings.com/leagues/football/88670775?category=halves', 'Q:T'],
+                 ['U1','NBA','https://sportsbook.draftkings.com/leagues/basketball/88670846', 'V:Y'],
+                 ['Z1','NBA Halves','https://sportsbook.draftkings.com/leagues/basketball/88670846?category=halves', 'AA:AD'],
+                 ['AE1','CBB','https://sportsbook.draftkings.com/leagues/basketball/88670771', 'AF:AI'],
+                 ['AJ1','CBB Halves','https://sportsbook.draftkings.com/leagues/basketball/88670771?category=halves', 'AK:AN']]
 
-    time.sleep(10)
+while 0 == 0:
+    
+    for item in scraping_list:
+        print('Starting', item[1])
+        worksheet.update(item[0], item[1])
+        obj = SportStatic(item[2])
+        obj.collectData()
+        df = obj.displayData()
+        worksheet.update(item[3], [df.columns.values.tolist()] + df.values.tolist())
+        print('Updated', item[1])
