4b4e55b3e6bb0c31afc457e0fafe3b5d12c9ee15
Jonah Liss <52020219+jonahliss@users.noreply.github.com>
2021-12-22 17:05:47 -0500

    Update Bluecoin_Web_Scraper.py

--- Bluecoin_Web_Scraper.py
+++ Bluecoin_Web_Scraper.py
@@ -18,6 +18,9 @@ def getRange(index):
         return chr(64 + (index // 26)) + chr(65 + (index % 26))
     return chr(65 + index)
 
+
+# PURPOSE: removes all special chracters from the key
+# PURPOSE: makes the key lowercase
 def formatKey(key):
     key = key.lower()
     key = key.replace("(", "")
@@ -34,7 +37,9 @@ def formatKey(key):
     key = key.replace("b ", "basketball")
     key = key.replace("fb", "football")
     key = key.replace("lines", "")
+    key = key.replace("nan", "NaN")
     return key
+
 
 class SportDynamic:
     def __init__(self, url):
@@ -110,31 +115,35 @@ class SportDynamic:
             # variables to help identify new team matchups
             prevID = -1
             isNew = True
+            
+            teamName = "NaN"
+            spread = "NaN"
+            odds = "NaN"
+            moneyline = "NaN"
+            
             if len(tableRows) > 1:
+
                 for row in tableRows:
                     cols = row.select('td')
                     # gets the eventID
                     try:
                         eventID = cols[2].text
+                        teamName = cols[3].text
                     except:
                         # if eventID not found, it is not a valid event
                         continue
-                    try:
-                        teamName = cols[3].text
-                    except:
-                        teamName = "NaN"
-                    try:
-                        spread = cols[4].text
-                    except:
-                        spread = "NaN"
-                    try:
-                        odds = cols[5].text
-                    except:
-                        odds = "NaN"
-                    try:
-                        moneyline = cols[6].text
-                    except:
-                        moneyline = "NaN"
+                        
+                    for i in range(4, 7):
+                        try:
+                            temp = cols[i].text
+                            if 'o' in temp or 'u' in temp:
+                                odds = temp
+                            elif (temp.count('+') + temp.count('-')) == 2:
+                                spread = temp
+                            elif (temp.count('+') + temp.count('-')) == 1:
+                                moneyline = temp
+                        except:
+                            pass
 
                     # if the eventID is "new", create new eventName, and reset the bettingData
                     if int(eventID) - 1 != prevID and isNew:
@@ -162,22 +171,20 @@ class SportDynamic:
                     cols = row.select('td')
                     bettingData = {"team": [], "spread": [], "odds": [], "moneyline": []}
                     eventName = cols[4].text
-                    try:
-                        teamName = cols[4].text
-                    except:
-                        teamName = "NaN"
-                    try:
-                        spread = cols[6].text
-                    except:
-                        spread = "NaN"
-                    try:
-                        odds = cols[7].text
-                    except:
-                        odds = "NaN"
-                    try:
-                        moneyline = cols[5].text
-                    except:
-                        moneyline = "NaN"
+                    teamName = cols[4].text
+                    
+                    for i in range(5, 8):
+                        try:
+                            temp = cols[i].text
+
+                            if 'o' in temp or 'u' in temp:
+                                odds = temp
+                            elif (temp.count('+') + temp.count('-')) == 2:
+                                spread = temp
+                            elif (temp.count('+') + temp.count('-')) == 1:
+                                moneyline = temp
+                        except:
+                            pass
 
                     bettingData["team"].append(teamName)
                     bettingData["spread"].append(spread)
@@ -203,18 +210,21 @@ class SportDynamic:
         self.navigateDriver()
         self.retrieveData()
         self.sortData()
+        
 
-# %%
 gc = gspread.service_account(filename='credentials.json')
+sh = gc.open("BettingScraper")
 print("Connected to Google Sheet")
 
-sh = gc.open("BettingScraper")
 
 website = SportDynamic('http://bluecoin.ag/core/mobile/')
 website.collectData()
 
-while True:
 
+startTime = time.perf_counter() 
+
+while True:
+    
     print('Starting')
 
     startingIndexCBB = 2400
@@ -222,6 +232,7 @@ while True:
     startingIndexCFB = 2400
     startingIndexNFL = 2400
     startingIndex = 2400
+    
     for key in website.allBets:
         ubdfNFL = website.displayData(key)
         key = formatKey(key)
@@ -229,38 +240,57 @@ while True:
         if 'ncaa' in bagOfWords and 'basketball' in bagOfWords:
             print('NCAA Basketball')
             worksheet = sh.get_worksheet(1)
+            worksheetNumber = 1
             startingIndexCBB += 5
             startingIndex = startingIndexCBB
         elif 'nba' in bagOfWords:
             print('NBA')
             worksheet = sh.get_worksheet(2)
+            worksheetNumber = 2
             startingIndexNBA += 5
             startingIndex = startingIndexNBA
         elif 'ncaa' in bagOfWords and 'football' in bagOfWords:
             print('NCAA Football')
             worksheet = sh.get_worksheet(3)
+            worksheetNumber = 3
             startingIndexCFB += 5
             startingIndex = startingIndexCFB
         elif 'nfl' in bagOfWords:
             print('NFL')
             worksheet = sh.get_worksheet(4)
+            worksheetNumber = 4
             startingIndexNFL += 5
             startingIndex = startingIndexNFL
         else:
             continue
-        worksheet.update(getRange(startingIndex - 5) + str(1), [[key], ["Bluecoin"]])
-        worksheet.update(getRange(startingIndex - 4) + ':' + getRange(startingIndex - 1),
-                         [ubdfNFL.columns.values.tolist()] + ubdfNFL.values.tolist())
-        worksheet.format(getRange(startingIndex - 5) + ':' + getRange(startingIndex - 1), {
-            "backgroundColor": {
-                "red": 0.25,
-                "green": 0.5,
-                "blue": 0.95
-            },
-        })
+            
+        try:
+            worksheet.update(getRange(startingIndex - 5) + str(1), [[key], ["Bluecoin"]])
+            worksheet.update(getRange(startingIndex - 4) + ':' + getRange(startingIndex - 1),
+                             [ubdfNFL.columns.values.tolist()] + ubdfNFL.values.tolist())
+        except:
+            
+            failTime = time.perf_counter()
+            pause = 101 - (failTime - startTime)
+            
+            print('Pausing for', pause, 'seconds')
+            time.sleep(pause)
+            print('Resuming')
+            
+            gc = gspread.service_account(filename='credentials.json')
+            sh = gc.open("BettingScraper")
+            
+            worksheet = sh.get_worksheet(worksheetNumber)
+            
+            startTime = time.perf_counter() 
+            
+            worksheet.update(getRange(startingIndex - 5) + str(1), [[key], ["Bluecoin"]])
+            worksheet.update(getRange(startingIndex - 4) + ':' + getRange(startingIndex - 1),
+                             [ubdfNFL.columns.values.tolist()] + ubdfNFL.values.tolist())
+
 
     print('Updated')
-
-    time.sleep(30)
-
+    
     website.driver.find_element(By.NAME, "ctl00$WagerContent$ctl01").click()
+    website.retrieveData()
+    website.sortData()
