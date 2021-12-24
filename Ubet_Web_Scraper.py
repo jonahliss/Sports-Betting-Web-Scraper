292996c2af9b01d8f0058f40254c94949cd2531b
Jonah Liss <52020219+jonahliss@users.noreply.github.com>
2021-12-24 00:18:16 -0500

    Update Ubet_Web_Scraper.py

--- Ubet_Web_Scraper.py
+++ Ubet_Web_Scraper.py
@@ -1,10 +1,44 @@
+import math
+import time
 import requests
 import gspread
-from bs4 import BeautifulSoup
 import pandas as pd
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+
+
+# Function to help organize Google Sheet
+def getRange(index):
+    if (index - 26) / math.pow(26, 2) >= 1:
+        return chr(64 + (index // int(math.pow(26, 2)))) + chr(
+            65 + (((index - 26) % int(math.pow(26, 2))) // 26)) + chr(
+            65 + (index % 26))
+    if index / 26 >= 1:
+        return chr(64 + (index // 26)) + chr(65 + (index % 26))
+    return chr(65 + index)
+
+
+# Function to classify HTML class text as a team's name, spread, odds, or moneyline
+def determineChild(child):
+    global teamName, spread, odds, moneyline
+    teamName, spread, odds, moneyline = 'NaN', 'NaN', 'NaN', 'NaN'
+    for i in range(4):
+        try:
+            temp = child.find_all('label')[i].text
+
+            if temp == child.find_all('label')[0].text:
+                teamName = temp
+            elif 'o' in temp or 'u' in temp:
+                odds = temp
+            elif (temp.count('+') + temp.count('-')) == 2:
+                spread = temp
+            elif (temp.count('+') + temp.count('-')) == 1:
+                moneyline = temp
+        except:
+            pass
+    return teamName, spread, odds, moneyline
 
 
 # Function to standardize text formatting across websites
@@ -24,140 +58,189 @@ def formatKey(key):
     key = key.replace("b ", "basketball")
     key = key.replace("fb", "football")
     key = key.replace("lines", "")
-    key = key.replace("o\xa0", "o")
-    key = key.replace("u\xa0", "u")
     key = key.replace("nan", "NaN")
     return key
 
 
-# Function to classify HTML class text as a team's name, spread, odds, or moneyline
-def determineChild(child):
-    global teamName, spread, odds, moneyline
-    teamName, spread, odds, moneyline = 'NaN', 'NaN', 'NaN', 'NaN'
-    try:
-        teamName = child.find_all(class_='event-cell__name-text')[0].text
-    except:
-        pass
-    for i in range(3):
-        try:
-            temp = child.find_all(class_='sportsbook-outcome-body-wrapper')[i].text
-            if 'O' in temp or 'U' in temp:
-                odds = temp
-            elif (temp.count('+') + temp.count('-')) == 2:
-                spread = temp
-            elif (temp.count('+') + temp.count('-')) == 1:
-                moneyline = temp
-        except:
-            pass
-    return teamName, spread, odds, moneyline
-
-
 # Class to retrieve, sort, and return website data
-class SportStatic:
-    def __init__(self, URL):
-        self.url = URL
-        self.teams_list = []
-        self.spreads_list = []
-        self.odds_list = []
-        self.moneylines_list = []
-        self.df = []
+class SportDynamic:
+    def __init__(self, url, options):
+        self.url = url
+        self.options = options
+        self.soup = ""
+        self.allBets = {}
+
+    def launchDriver(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--disable-dev-shm-usage')
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.get(self.url)
+
+    def enterDriver(self):
+        self.driver.find_element(By.NAME, "Account").send_keys("Efind1")
+        self.driver.find_element(By.NAME, "Password").send_keys("123")
+        self.driver.find_element(By.XPATH, '//button[text()=" Login"]').click()
+
+    def navigateDriver(self):
+        self.driver.find_element(By.ID, "ctl00_lnkSports").click()
+        
+        leagues = []
+        if 'CBB' in self.options:
+            leagues.append(self.driver.find_element(By.CSS_SELECTOR, '#sport_COLLEGEBASKETBALL'))
+        if 'NBA' in self.options:
+            leagues.append(self.driver.find_element(By.CSS_SELECTOR, '#sport_BASKETBALL'))
+        if 'CFB' in self.options:
+            leagues.append(self.driver.find_element(By.CSS_SELECTOR, '#sport_COLLEGEFOOTBALL'))
+        if 'NFL' in self.options:
+            leagues.append(self.driver.find_element(By.CSS_SELECTOR, '#sport_FOOTBALL'))
+        
+        for button in leagues:
+            button = button.find_element(By.CSS_SELECTOR, '.divLeagueContainer > a')
+            if button.text == "Check All":
+                button.click()
+            if button.text == "Check All":
+                button.click()
+
+        self.driver.find_element(By.ID, "continue-2").click()
 
     def retrieveData(self):
-        driver.get(self.url)
-        page = driver.page_source
-        self.soup = BeautifulSoup(page, "html.parser")
+        html = self.driver.page_source
+        self.soup = BeautifulSoup(html, "html.parser")
+        '''self.driver.quit()'''
 
     def sortData(self):
-        for event in self.soup.select('div.parlay-card-10-a'):
-            children = event.find_all('tr')
-            for child in children[1:]:
-                determineChild(child)
-                self.teams_list.append(formatKey(teamName))
-                self.spreads_list.append(formatKey(spread))
-                self.odds_list.append(formatKey(odds))
-                self.moneylines_list.append(formatKey(moneyline))
+        for event in self.soup.select('div.line'):
+            eventType = event.select('h4')[0].text
 
-    def displayData(self):
+            if not eventType in self.allBets:
+                self.allBets[eventType] = []
+
+            tempEvent = {}
+            
+            # gets the name of the event as a html tag
+            eventName = event.select('div > div > h6')[0]
+            tempEvent[eventName.text] = {}
+
+            # gets the team and spreads of the event
+            bettingData = {"team": [], "spread": [], "odds": [], "moneyline": []}
+            children = event.find_all('div', class_='row py-4')
+            
+            for child in children[1:-1]:
+                determineChild(child)
+                bettingData["team"].append(formatKey(teamName))
+                bettingData["spread"].append(formatKey(spread))
+                bettingData["odds"].append(formatKey(odds))
+                bettingData["moneyline"].append(formatKey(moneyline))
+
+            # sets the betting data of the temporary event
+            tempEvent[eventName.text] = bettingData
+
+            # puts the event into the list of all events
+            self.allBets[eventType].append(tempEvent)
+
+    def displayData(self, sport):
+        df = pd.DataFrame()
+        for event in self.allBets[sport]:
+            for item in event:
+                df = pd.concat([df, pd.DataFrame({'Teams': event[item]['team'], 'Spreads': event[item]['spread'],
+                                                  'Odds': event[item]['odds'],
+                                                  'Moneyline': event[item]['moneyline']})], axis=0)
+        df = df.fillna('')
+        return df
+
+    def collectData(self):
+        self.launchDriver()
+        self.enterDriver()
+        self.navigateDriver()
         self.retrieveData()
         self.sortData()
-        self.df = pd.DataFrame()
-        self.df = pd.concat([self.df, pd.DataFrame({'Team': self.teams_list}), pd.DataFrame({'Spread': self.spreads_list}),
-                             pd.DataFrame({'Odds': self.odds_list}), pd.DataFrame({'Moneyline': self.moneylines_list})], axis=1)
-        self.df = self.df.fillna('')
-        return self.df
-
-    
-# Launching automated Chrome Browser
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
+        
 
 # Establishing connection with Google Sheets
 gc = gspread.service_account(filename='credentials.json')
 sh = gc.open("BettingScraper")
 
 
-# Initializing data used to execute scraping process
-scraping_list_NFL = [[4,'A1','nfl','football/88670561','B:E'],
-                 [4,'F1','nfl 1st half','football/88670561?category=halves&subcategory=1st-half','G:J'],
-                 [4,'K1','nfl 2nd half','football/88670561?category=halves&subcategory=2nd-half','L:O'],
-                 [4,'P1','nfl 1st quarter','football/88670561?category=quarters&subcategory=1st-quarter','Q:T'],
-                 [4,'U1','nfl 2nd quarter','football/88670561?category=quarters&subcategory=2nd-quarter','V:Y'],
-                 [4,'Z1','nfl 3rd quarter','football/88670561?category=quarters&subcategory=3rd-quarter','AA:AD'],
-                 [4,'AE1','nfl 4th quarter','football/88670561?category=quarters&subcategory=4th-quarter','AF:AI']]
-                 
-scraping_list_CFB = [[3,'A1','ncaa football','football/88670775','B:E'],
-                 [3,'F1','ncaa football 1st half','football/88670775?category=halves&subcategory=1st-half','G:J'],
-                 [3,'K1','ncaa football 2nd half','football/88670775?category=halves&subcategory=2nd-half','L:O'],
-                 [3,'P1','ncaa football 1st quarter','football/88670775?category=quarters&subcategory=1st-quarter','Q:T'],
-                 [3,'U1','ncaa football 2nd quarter','football/88670775?category=quarters&subcategory=2nd-quarter','V:Y'],
-                 [3,'Z1','ncaa football 3rd quarter','football/88670775?category=quarters&subcategory=3rd-quarter','AA:AD'],
-                 [3,'AE1','ncaa football 4th quarter','football/88670775?category=quarters&subcategory=4th-quarter','AF:AI']]
-                 
-scraping_list_NBA = [[2,'A1','nba','basketball/88670846','B:E'],
-                 [2,'F1','nba 1st half','basketball/88670846?category=halves&subcategory=1st-half','G:J'],
-                 [2,'K1','nba 2nd half','basketball/88670846?category=halves&subcategory=2nd-half','L:O'],
-                 [2,'P1','nba 1st quarter','basketball/88670846?category=quarters&subcategory=1st-quarter','Q:T'],
-                 [2,'U1','nba 2nd quarter','basketball/88670846?category=quarters&subcategory=2nd-quarter','V:Y'],
-                 [2,'Z1','nba 3rd quarter','basketball/88670846?category=quarters&subcategory=3rd-quarter','AA:AD'],
-                 [2,'AE1','nba 4th quarter','basketball/88670846?category=quarters&subcategory=4th-quarter','AF:AI']]
-                 
-scraping_list_CBB = [[1,'A1','ncaa basketball','basketball/88670771','B:E',1],
-                 [1,'F1','ncaa basketball 1st half','basketball/88670771?category=halves&subcategory=1st-half','G:J'],
-                 [1,'K1','ncaa basketball 2nd half','basketball/88670771?category=halves&subcategory=2nd-half','L:O'],
-                 [1,'P1','ncaa basketball 1st quarter','basketball/88670771?category=quarters&subcategory=1st-quarter','Q:T'],
-                 [1,'U1','ncaa basketball 2nd quarter','basketball/88670771?category=quarters&subcategory=2nd-quarter','V:Y'],
-                 [1,'Z1','ncaa basketball 3rd quarter','basketball/88670771?category=quarters&subcategory=3rd-quarter','AA:AD'],
-                 [1,'AE1','ncaa basketball 4th quarter','basketball/88670771?category=quarters&subcategory=4th-quarter','AF:AI']]
-
-
 # Determining sports and leageus to scrape
 options = input('CBB, NBA, CFB, NFL\nWhat data do you want to scrape? ')
-scraping_list = []
-if 'CBB' in options:
-    scraping_list += scraping_list_CBB
-if 'NBA' in options:
-    scraping_list += scraping_list_NBA
-if 'CFB' in options:
-    scraping_list += scraping_list_CFB
-if 'NFL' in options:
-    scraping_list += scraping_list_NFL
-    
+
 
 # Executing scraping process
+website = SportDynamic('https://ubet.ag/', options)
+website.collectData()
+
+startTime = time.perf_counter() 
+
 while True:
-    for item in scraping_list:
-    # Select Appropriate Spreadsheet
-        worksheet = sh.get_worksheet(item[0])
-    # Write Title onto Spreadsheet
-        print('Starting', item[2])
-        worksheet.update(item[1], item[2])
-    # Collect Data for Title 
-        obj = SportStatic('https://sportsbook.draftkings.com/leagues/'+item[3])
-        df = obj.displayData()
-    # Insert Data into Spreadsheet
-        worksheet.update(item[4], [df.columns.values.tolist()] + df.values.tolist())
-        print('Updated', item[2])
+    
+    print('Starting')
+
+    startingIndexCBB = 1800
+    startingIndexNBA = 1800
+    startingIndexCFB = 1800
+    startingIndexNFL = 1800
+    startingIndex = 1800
+    
+    for key in website.allBets:
+        ubdfNFL = website.displayData(key)
+        key = formatKey(key)
+        bagOfWords = key.split()
+        if 'ncaa' in bagOfWords and 'basketball' in bagOfWords:
+            print('NCAA Basketball')
+            worksheet = sh.get_worksheet(1)
+            worksheetNumber = 1
+            startingIndexCBB += 5
+            startingIndex = startingIndexCBB
+        elif 'nba' in bagOfWords:
+            print('NBA')
+            worksheet = sh.get_worksheet(2)
+            worksheetNumber = 2
+            startingIndexNBA += 5
+            startingIndex = startingIndexNBA
+        elif 'ncaa' in bagOfWords and 'football' in bagOfWords:
+            print('NCAA Football')
+            worksheet = sh.get_worksheet(3)
+            worksheetNumber = 3
+            startingIndexCFB += 5
+            startingIndex = startingIndexCFB
+        elif 'nfl' in bagOfWords:
+            print('NFL')
+            worksheet = sh.get_worksheet(4)
+            worksheetNumber = 4
+            startingIndexNFL += 5
+            startingIndex = startingIndexNFL
+        else:
+            continue
+            
+        try:
+            worksheet.update(getRange(startingIndex - 5) + str(1), [[key], ["Ubet"]])
+            worksheet.update(getRange(startingIndex - 4) + ':' + getRange(startingIndex - 1),
+                             [ubdfNFL.columns.values.tolist()] + ubdfNFL.values.tolist())
+        except:
+            
+            failTime = time.perf_counter()
+            pause = 101 - (failTime - startTime)
+            
+            print('Pausing for', pause, 'seconds')
+            time.sleep(pause)
+            print('Resuming')
+            
+            gc = gspread.service_account(filename='credentials.json')
+            sh = gc.open("BettingScraper")
+            
+            worksheet = sh.get_worksheet(worksheetNumber)
+            
+            startTime = time.perf_counter() 
+            
+            worksheet.update(getRange(startingIndex - 5) + str(1), [[key], ["Ubet"]])
+            worksheet.update(getRange(startingIndex - 4) + ':' + getRange(startingIndex - 1),
+                             [ubdfNFL.columns.values.tolist()] + ubdfNFL.values.tolist())
+
+
+    print('Updated')
+    
+    website.driver.find_element(By.NAME, "ctl00$WagerContent$ctl01").click()
+    website.retrieveData()
+    website.sortData()
